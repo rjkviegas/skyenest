286e3f4be8a1582ea5653601ae7ea29a8a744aa6
Ryan Viegas <66323087+rjkviegas@users.noreply.github.com>
2020-11-24 14:03:45 +0000

    Install supertest and app module test

--- skyenest.js
+++ skyenest.js
@@ -1,37 +1,6 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-
-const app = express();
-
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+const app = require('./lib/app');
 
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Skyenest');
-});
-
-app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Skyenest');
-});
-
-app.use('404', (req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
-
-app.use('500', (err, req, res, next) => {
-    console.error(err.message);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
-});
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
